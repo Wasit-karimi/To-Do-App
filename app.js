463a1ecc21acab2bd3424b32fc943b738e0102bf
Wasit-karimi <wasitkarimi0@gmail.com>
2025-09-05 11:34:23 +0430

    correct the regex

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ form.addEventListener("submit", (e) => {
   err.textContent = "";
 
   //stored regex condition in a variable
-  const taskPattern = /^[A-Za-z]{10,40}$/;
+  const taskPattern = /^[A-Za-z ]{10,40}$/;
 
   //get the task value from input
   const task = formData.get("task").trim();
